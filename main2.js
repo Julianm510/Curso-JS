d32a0cfe27bbfbc562604a15dddfb194f7ff659c
Julian <julianmuriel510@gmail.com>
2023-12-08 18:47:42 -0300

    tercer entrega

--- main2.js
+++ main2.js
@@ -32,6 +32,7 @@ if (localStorage.getItem("alumnos")) {
 function filtrarAlumnos() {
     const body = document.querySelector("body")
     const input = document.getElementById("filtrarA").value
+
     const palabraClave = input.trim().toUpperCase()
     const resultado = lista.filter((alumno) => alumno.nombre.toUpperCase().includes(palabraClave))
 
@@ -46,6 +47,8 @@ function filtrarAlumnos() {
             const nombre = document.createElement("h2")
             nombre.textContent = `Nombre: ${alumno.nombre}`
             card.appendChild(nombre)
+            nombre.classList.add("nombre")
+
 
             const apellido = document.createElement("h2")
             apellido.textContent = `Apellido: ${alumno.apellido}`
@@ -66,6 +69,79 @@ function filtrarAlumnos() {
 
 
 
+function agregarAlumno() {
+
+    const form = document.createElement("form")
+    form.innerHTML = `
+    <label for="nombre-input">Nombre:</label>
+    <input id= "nombre-input" type="text" step="0.01" required>
+    
+    <label for="apellido-input">Apellido:</label>
+    <input id= "apellido-input" type="text" step="0.01" required>
+
+    <label for="nota-input">Nota:</label>
+    <input id= "nota-input" type="number" step="0.01" required>
+
+    <button type="submit">Agregar</button>
+    `
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const nombreInput = document.getElementById("nombre-input").value.trim()
+        const apellidoInput = document.getElementById("apellido-input").value.trim()
+        const notaInput = parseInt(document.getElementById("nota-input").value)
+
+        if (isNaN(notaInput) || nombreInput === "" || apellidoInput === "") {
+            alert("por favor ingresa valores validos.")
+            return
+        }
+
+        const alumno = new Alumno(nombreInput, apellidoInput, notaInput)
+
+        if (lista.some((elemento) => elemento.nombre === alumno.nombre && elemento.apellido === alumno.apellido )) {
+            alert("el alumno ya existe")
+            return
+        }
+
+        lista.push(alumno)
+
+        localStorage.setItem("alumnos", JSON.stringify(lista))
+        alert("se agrego el alumno " + alumno.nombre + " " + alumno.apellido + " a la lista")
+
+
+
+        const container = document.createElement("div")
+
+        lista.forEach((alumno) => {
+            const card = document.createElement("div")
+
+            const nombre = document.createElement("h2")
+            nombre.textContent = `Nombre: ${alumno.nombre}`
+            card.appendChild(nombre)
+
+            const apellido = document.createElement("h2")
+            apellido.textContent = `Apellido: ${alumno.apellido}`
+            card.appendChild(apellido)
+
+            const nota = document.createElement("h2")
+            nota.textContent = `Nota: ${alumno.nota}`
+            card.appendChild(nota)
+            container.appendChild(card)
+        })
+
+        const body = document.querySelector("body")
+        body.appendChild(container)
+
+        form.reset()
+
+    })
+
+    const body = document.querySelector("body")
+    body.appendChild(form)
+
+}
+
 
 
 const filtrarBtn = document.getElementById("filtrar")
@@ -73,7 +149,7 @@ filtrarBtn.classList.add("button")
 filtrarBtn.addEventListener("click", filtrarAlumnos)
 
 
-const agregarBtn = document.getElementById("agregarAlumno")
-agregarAlumnoBtn.classList.add("botonera")
-agregarBtn.addEventListener("click", filtrarAlumnos)
+const agregarAlumnoBtn = document.getElementById("agregarAlumno")
+agregarAlumnoBtn.classList.add("button")
+agregarAlumnoBtn.addEventListener("click", agregarAlumno)
 
